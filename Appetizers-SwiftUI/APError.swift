5d0d3591f70efb65a235f15f09da4d6a0bccbc9c
Mahan Kheirollahi <kheirollahi.mahan@gmail.com>
2024-06-24 00:16:01 +0300

    Add APError enum

---
+++ Appetizers-SwiftUI/APError.swift
@@ -0,0 +1,15 @@
+//
+//  APError.swift
+//  Appetizers-SwiftUI
+//
+//  Created by Mahan Kheirollahi on 6/23/24.
+//
+
+import Foundation
+
+enum APError : Error {
+    case invalidURL
+    case invalidResponse
+    case invalidData
+    case unableToComplete
+}
