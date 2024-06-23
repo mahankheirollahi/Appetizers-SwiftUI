6628da0d2d799e1ef5d733cade3dfeea8d8e20db
Mahan Kheirollahi <kheirollahi.mahan@gmail.com>
2024-06-23 20:02:59 +0300

    Create order view

---
+++ Appetizers-SwiftUI/OrderView.swift
@@ -0,0 +1,21 @@
+//
+//  OrderView.swift
+//  Appetizers-SwiftUI
+//
+//  Created by Mahan Kheirollahi on 6/23/24.
+//
+
+import SwiftUI
+
+struct OrderView: View {
+    var body: some View {
+        NavigationView{
+            Text("Orders")
+                .navigationTitle("Orders")
+        }
+    }
+}
+
+#Preview {
+    OrderView()
+}
