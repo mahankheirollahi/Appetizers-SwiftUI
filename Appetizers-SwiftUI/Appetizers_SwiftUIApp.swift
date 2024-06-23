dae264922b634128180aa5822aa028b58723496f
Mahan Kheirollahi <kheirollahi.mahan@gmail.com>
2024-06-23 19:48:29 +0300

    Initial Commit

---
+++ Appetizers-SwiftUI/Appetizers_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Appetizers_SwiftUIApp.swift
+//  Appetizers-SwiftUI
+//
+//  Created by Mahan Kheirollahi on 6/23/24.
+//
+
+import SwiftUI
+
+@main
+struct Appetizers_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
