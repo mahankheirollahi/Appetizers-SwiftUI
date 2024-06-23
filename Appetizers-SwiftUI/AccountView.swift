5a18fa5510083959ce5395e6b566dfee017857dc
Mahan Kheirollahi <kheirollahi.mahan@gmail.com>
2024-06-23 20:02:40 +0300

    Create account view

---
+++ Appetizers-SwiftUI/AccountView.swift
@@ -0,0 +1,21 @@
+//
+//  AccountView.swift
+//  Appetizers-SwiftUI
+//
+//  Created by Mahan Kheirollahi on 6/23/24.
+//
+
+import SwiftUI
+
+struct AccountView: View {
+    var body: some View {
+        NavigationView{
+            Text("Account")
+                .navigationTitle("Account")
+        }
+    }
+}
+
+#Preview {
+    AccountView()
+}
