1ddd75baf3e31f3ebffea26da4645c88d763debc
Mahan Kheirollahi <kheirollahi.mahan@gmail.com>
2024-06-23 20:02:52 +0300

    Create appetizers view

---
+++ Appetizers-SwiftUI/AppetizersView.swift
@@ -0,0 +1,21 @@
+//
+//  AppetizersView.swift
+//  Appetizers-SwiftUI
+//
+//  Created by Mahan Kheirollahi on 6/23/24.
+//
+
+import SwiftUI
+
+struct AppetizersView: View {
+    var body: some View {
+        NavigationView{
+            Text("Appetizers")
+                .navigationTitle("Appetizers")
+        }
+    }
+}
+
+#Preview {
+    AppetizersView()
+}
