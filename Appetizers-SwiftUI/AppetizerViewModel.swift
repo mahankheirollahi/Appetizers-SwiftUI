1c3b8298d0ca33e0280c580767576a340c07ed73
Mahan Kheirollahi <kheirollahi.mahan@gmail.com>
2024-06-24 08:12:16 +0300

    Add error handler

--- Appetizers-SwiftUI/AppetizerViewModel.swift
+++ Appetizers-SwiftUI/AppetizerViewModel.swift
@@ -11,6 +11,7 @@ import SwiftUI
 final class AppetizerViewModel: ObservableObject{
     
     @Published var appetizers: [Appetizer] = []
+    @Published var alertItem: AlertItem?
     
     func getAppetizers (){
         NetworkManager.shared.getAppetizers{ result in
@@ -19,11 +20,20 @@ final class AppetizerViewModel: ObservableObject{
                 case .success(let appetizers):
                     self.appetizers = appetizers
                 case .failure(let error):
-                    print(error.localizedDescription)
+                    switch error{
+                    case .invalidResponse:
+                        self.alertItem = AlertContext.invalidResponse
+                    case .invalidURL:
+                        self.alertItem = AlertContext.invalidURL
+                    case .invalidData:
+                        self.alertItem =  AlertContext.invalidData
+                    case .unableToComplete:
+                        self.alertItem = AlertContext.unableToComplete
+                    }
                     
                 }
             }
-           
+            
         }
     }
 }
