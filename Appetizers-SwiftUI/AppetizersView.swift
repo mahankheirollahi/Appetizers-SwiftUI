64f2cc57e93c654f3e688a9324d7d334d5f420fb
Mahan Kheirollahi <kheirollahi.mahan@gmail.com>
2024-06-24 08:12:51 +0300

    Add error handler

--- Appetizers-SwiftUI/AppetizersView.swift
+++ Appetizers-SwiftUI/AppetizersView.swift
@@ -20,9 +20,13 @@ struct AppetizersView: View {
         }
         .onAppear{
             viewModel.getAppetizers()
+        }
+        .alert(item: $viewModel.alertItem){alertItem in
+            Alert(title: alertItem.title,
+                  message: alertItem.message,
+                  dismissButton: alertItem.dismissButton)
+        }
     }
-    }
-  
 }
 
 #Preview {
