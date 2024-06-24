ec0cd4afba4a0c7d349c11166d43c822c4454928
Mahan Kheirollahi <kheirollahi.mahan@gmail.com>
2024-06-24 08:50:12 +0300

    Add isLoading property

--- Appetizers-SwiftUI/AppetizerViewModel.swift
+++ Appetizers-SwiftUI/AppetizerViewModel.swift
@@ -12,10 +12,13 @@ final class AppetizerViewModel: ObservableObject{
     
     @Published var appetizers: [Appetizer] = []
     @Published var alertItem: AlertItem?
+    @Published var isLoading = false
     
     func getAppetizers (){
+        isLoading = true
         NetworkManager.shared.getAppetizers{ result in
             DispatchQueue.main.async{
+                self.isLoading = false
                 switch result{
                 case .success(let appetizers):
                     self.appetizers = appetizers
