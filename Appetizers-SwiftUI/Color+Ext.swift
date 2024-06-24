acf942d9005697ba0e99b6ad4de71810f3d55be4
Mahan Kheirollahi <kheirollahi.mahan@gmail.com>
2024-06-24 08:49:58 +0300

    Add color extensions

---
+++ Appetizers-SwiftUI/Color+Ext.swift
@@ -0,0 +1,16 @@
+//
+//  Color+Ext.swift
+//  Appetizers-SwiftUI
+//
+//  Created by Mahan Kheirollahi on 6/24/24.
+//
+
+import SwiftUI
+
+extension Color{
+    static let brandPrimary = Color("primary")
+}
+
+extension UIColor{
+    static let brandPrimary = UIColor(named:"primary")
+}
