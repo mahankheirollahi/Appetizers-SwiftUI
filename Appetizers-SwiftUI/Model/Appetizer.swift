3086226136e364f0748d33011bf3739e470d5853
Mahan Kheirollahi <kheirollahi.mahan@gmail.com>
2024-06-26 00:17:08 +0300

    Organize project files

---
+++ Appetizers-SwiftUI/Model/Appetizer.swift
@@ -0,0 +1,36 @@
+//
+//  Appetizer.swift
+//  Appetizers-SwiftUI
+//
+//  Created by Mahan Kheirollahi on 6/23/24.
+//
+
+import Foundation
+
+struct Appetizer: Decodable,Identifiable {
+    let id: Int
+    let name: String
+    let description: String
+    let price: Double
+    let imageURL: String
+    let calories: Int
+    let protein: Int
+    let carbs: Int
+}
+
+struct AppetizerResponse: Decodable {
+    let request: [Appetizer]
+}
+
+struct MockData{
+    static let sampleAppetizer = Appetizer(id: 0001,
+                                      name: "Test 1",
+                                      description: "Some Description for appetizer",
+                                      price: 9.99,
+                                      imageURL: "",
+                                      calories: 99,
+                                      protein: 99,
+                                      carbs: 99)
+    
+    static let appetizers = [sampleAppetizer,sampleAppetizer,sampleAppetizer,sampleAppetizer]
+}
