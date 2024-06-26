0831713f2b482ca86682576e6c3d24c30480a7f7
Mahan Kheirollahi <kheirollahi.mahan@gmail.com>
2024-06-26 15:48:01 +0300

    Add personal info and Request section

--- Appetizers-SwiftUI/Screens/AccountView/AccountView.swift
+++ Appetizers-SwiftUI/Screens/AccountView/AccountView.swift
@@ -8,10 +8,40 @@
 import SwiftUI
 
 struct AccountView: View {
+    
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var email = ""
+    @State private var birthdate = Date()
+    @State private var extraNapkins = false
+    @State private var frequentRefills = false
+    
+    
     var body: some View {
         NavigationView{
-            Text("Account")
-                .navigationTitle("Account")
+            Form{
+                Section(header:Text("Personal Info")){
+                    TextField("First Name", text: $firstName)
+                        .autocorrectionDisabled()
+                    TextField("Last Name", text: $lastName)
+                        .autocorrectionDisabled()
+                    TextField("Email", text: $email)
+                        .keyboardType(.emailAddress)
+                        .autocorrectionDisabled()
+                        .textInputAutocapitalization(.never)
+                    DatePicker("Birthday", selection: $birthdate, displayedComponents: .date)
+                    Button{
+                        
+                    }label: {
+                        Text("Save Changes")
+                    }
+                }
+                
+                Section(header:Text("Request")){
+                    Toggle("Extra Napkins", isOn: $extraNapkins)
+                    Toggle("Frequent Refills", isOn: $frequentRefills)
+                }  .toggleStyle(SwitchToggleStyle(tint:.brandPrimary))
+            } .navigationTitle("Account")
         }
     }
 }
