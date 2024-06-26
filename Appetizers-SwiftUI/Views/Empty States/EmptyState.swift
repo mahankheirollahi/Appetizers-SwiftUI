f0c7f2bd3d3ffd325a4b47a2d676f5e557322f4b
Mahan Kheirollahi <kheirollahi.mahan@gmail.com>
2024-06-26 23:33:00 +0300

    Add empty state view

---
+++ Appetizers-SwiftUI/Views/Empty States/EmptyState.swift
@@ -0,0 +1,40 @@
+//
+//  EmptyState.swift
+//  Appetizers-SwiftUI
+//
+//  Created by Mahan Kheirollahi on 6/26/24.
+//
+
+import SwiftUI
+
+struct EmptyState: View {
+    
+    let imageName: String
+    let message: String
+    
+    var body: some View {
+        ZStack{
+            Color(.systemBackground)
+                .ignoresSafeArea(edges: .all)
+            
+            VStack{
+                Image(imageName)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(height: 150)
+                
+                Text(message)
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .multilineTextAlignment(.center)
+                    .foregroundStyle(.secondary)
+                    .padding()
+            }
+            .offset(y:-50)
+        }
+    }
+}
+
+#Preview {
+    EmptyState(imageName: "empty-order", message: "Test Message")
+}
