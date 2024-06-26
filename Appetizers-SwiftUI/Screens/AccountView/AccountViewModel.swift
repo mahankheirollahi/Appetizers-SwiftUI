3a09a1e24b5f4efee3791ce513abee7adb56add8
Mahan Kheirollahi <kheirollahi.mahan@gmail.com>
2024-06-26 19:23:06 +0300

    Add account view model

---
+++ Appetizers-SwiftUI/Screens/AccountView/AccountViewModel.swift
@@ -0,0 +1,42 @@
+//
+//  AccountViewModel.swift
+//  Appetizers-SwiftUI
+//
+//  Created by Mahan Kheirollahi on 6/26/24.
+//
+
+import SwiftUI
+
+final class AccountViewModel:ObservableObject{
+    
+   @Published var firstName = ""
+   @Published var lastName = ""
+   @Published var email = ""
+   @Published var birthdate = Date()
+   @Published var extraNapkins = false
+   @Published var frequentRefills = false
+    
+    @Published var alertItem: AlertItem?
+    
+    var isValidForm: Bool{
+        
+        guard !firstName.isEmpty && !lastName.isEmpty && !email.isEmpty else {
+            alertItem = AlertContext.invalidForm
+            return false
+        }
+        
+        guard email.isValidEmail else {
+            alertItem = AlertContext.invalidEmail
+            return false
+        }
+        
+        
+        
+        return true
+    }
+    
+    func saveChanges(){
+        guard isValidForm else {return}
+        print("Changes have been saved")
+    }
+}
