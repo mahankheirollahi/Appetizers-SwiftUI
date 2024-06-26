98ab7a86d030a2b7024b2a803cdb19f32d08f949
Mahan Kheirollahi <kheirollahi.mahan@gmail.com>
2024-06-26 23:15:29 +0300

    Add order screen ui with mock data

--- Appetizers-SwiftUI/Screens/OrderView/OrderView.swift
+++ Appetizers-SwiftUI/Screens/OrderView/OrderView.swift
@@ -8,11 +8,35 @@
 import SwiftUI
 
 struct OrderView: View {
+    
+    @State private var orderItems = MockData.appetizers
+    
     var body: some View {
-        NavigationView{
-            Text("Orders")
-                .navigationTitle("Orders")
-        }
+            NavigationView{
+                VStack{
+                    List{
+                        ForEach(orderItems){ apetizers in
+                            AppetizerListTile(appetizer: apetizers)
+                       }
+                        .onDelete(perform: { indexSet in
+                            deleteItems(at: indexSet)
+                        })
+                    }
+                    .listStyle(PlainListStyle())
+                    
+                    Button{
+                        
+                    }label: {
+                        APButton(title: "Order")
+                    }
+                    .padding(.bottom,25)
+                }
+                .navigationTitle("Order")
+            }
+    }
+    
+    func deleteItems(at offesets: IndexSet){
+        orderItems.remove(atOffsets: offesets)
     }
 }
 
